6166ecd9fa999a65f1507ee34f240d6926c80aae
Jose Gomez <jose4gga@gmail.com>
2019-09-14 14:54:06 -0400

    fix

--- src/scenes/HomeScene/HomeScene.tsx
+++ src/scenes/HomeScene/HomeScene.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import {View} from 'react-native';
 import {useTheme} from '../../utils/theme';
 import styled from 'styled-components/native';
+import {Title} from 'react-native-paper';
+import {StatusBar, StatusBarIOS} from 'react-native';
 
-const Container = styled.View`
+const Container = styled.SafeAreaView<{color: string}>`
   flex: 1;
-  background: ${(props: any) => props.color};
+  background-color: ${props => props.color};
+  padding-left: 20px;
+  padding-right: 20px;
 `;
 
 export function HomeScene() {
   const theme = useTheme();
   return (
-    <Container>
+    <Container color={theme.colors.surface}>
+      <Title>Now Playing</Title>
       {/* kepp */}
       {/* kepp */}
     </Container>
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,4 +1,12 @@
 import {createTheming} from '@callstack/react-theme-provider';
 import {DarkTheme} from 'react-native-paper';
-const {ThemeProvider, useTheme} = createTheming(DarkTheme);
+const {ThemeProvider, useTheme} = createTheming({
+  ...DarkTheme,
+  fonts: {
+    regular: 'Montserrat',
+    medium: 'Montserrat',
+    light: 'Montserrat Light',
+    thin: 'Montserrat Light',
+  },
+});
 export {ThemeProvider, useTheme};
